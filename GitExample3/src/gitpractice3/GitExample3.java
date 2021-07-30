3dd41b53bc6cc4507253fbb1f644dcc8a5299d7c
henry <henry@LAPTOP-0I87D7PT.attlocal.net>
2021-07-30 12:46:07 -0700

    Practing third times a charm

---
+++ GitExample3/src/gitpractice3/GitExample3.java
@@ -0,0 +1,13 @@
+package gitpractice3;
+
+public class GitExample3 {
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+
+	}
+
+}
